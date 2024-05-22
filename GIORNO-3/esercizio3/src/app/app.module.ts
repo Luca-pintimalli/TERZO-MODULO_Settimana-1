2272e5790b90e25fb5b7a091c4f1fdb14c93d38f
Luca-pintimalli <163635386+Luca-pintimalli@users.noreply.github.com>
2024-05-22 16:22:51 +0200

    .

---
+++ GIORNO-3/esercizio3/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ActivePostComponent } from './components/active-post/active-post.component';
+import { InactivePostComponent } from './components/inactive-post/inactive-post.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActivePostComponent,
+    InactivePostComponent,
+    PostDetailComponent,
+    NavBarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
